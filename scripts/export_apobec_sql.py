118cedb12195c5c6910eb057cf39661e89a59daf
Philip Tzou <philiptz@stanford.edu>
2019-08-14 14:41:47 -0700

    Update export_apobec_sql to correct table name

--- scripts/export_apobec_sql.py
+++ scripts/export_apobec_sql.py
@@ -5,21 +5,21 @@ from hivfacts import HIVAPOBEC
 def main():
     apobec = HIVAPOBEC()
     sql = [
-        'CREATE TABLE IF NOT EXISTS `tblLUAPOBEC2` (',
+        'CREATE TABLE IF NOT EXISTS `tblLUAPOBEC` (',
         "  `Gene` enum('PR','RT','IN') NOT NULL,",
         '  `Position` smallint(3) unsigned NOT NULL,',
         '  `AA` char(1) NOT NULL,',
         '  PRIMARY KEY (`Gene`,`Position`,`AA`)',
         ') ENGINE=MyISAM DEFAULT CHARSET=utf8;',
         '',
-        'TRUNCATE TABLE `tblLUAPOBEC2`;'
+        'TRUNCATE TABLE `tblLUAPOBEC`;'
         '',
         '',
     ]
 
     for mut in apobec.get_apobec_list():
         sql.append(
-            "INSERT INTO `tblLUAPOBEC2` (Gene, Position, AA) VALUES "
+            "INSERT INTO `tblLUAPOBEC` (Gene, Position, AA) VALUES "
             "('{gene}', {position}, '{aa}');"
             .format(**mut))
 
