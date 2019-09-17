ddf80ff42c4cc79f104e3ed5a7e1dc4a50b2b19c
Philip Tzou <philiptz@stanford.edu>
2019-09-16 17:11:32 -0700

    Update stat_apobec_unusual to exclude ins/del

--- scripts/stat_apobec_unusual.py
+++ scripts/stat_apobec_unusual.py
@@ -9,12 +9,18 @@ def main():
     matrix = [[0, 0], [0, 0]]
 
     for mutation in aapcnts:
+        if mutation['aa'] in '-_':
+            continue
         is_apobec = apobecs.is_apobec_mutation(
             mutation['gene'],
             mutation['position'],
             mutation['aa']
         )
         is_unusual = mutation['isUnusual']
+        if is_apobec and not is_unusual:
+            print(
+                mutation['gene'], mutation['position'],
+                mutation['aa'], mutation['percent'], sep='\t')
         matrix[is_apobec][is_unusual] += 1
 
     print('               (+) APOBEC    (-) APOBEC')
