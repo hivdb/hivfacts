4d93238057faf8001a9c251b90aac183a6b22692
Philip Tzou <philip.npc@gmail.com>
2022-06-02 16:09:28 -0700

    Prepare for releasing of 2022.6

--- hivfacts-python/hivfacts/__init__.py
+++ hivfacts-python/hivfacts/__init__.py
@@ -2,6 +2,6 @@ from .hivaapcnt import HIVAAPcnt
 from .hivcodonpcnt import HIVCodonPcnt
 from .hivapobec import HIVAPOBEC
 
-version = '2021.3'
+version = '2022.6a1'
 
 __all__ = ['HIVAAPcnt', 'HIVCodonPcnt', 'HIVAPOBEC']
